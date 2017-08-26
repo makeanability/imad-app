9ff973f6b378dad653c48b601f88da32aab7d21f
makeanability <makeanability@gmail.com>
2017-08-26 15:37:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
-app.get('/h', function (req, res) {
+app.get('/main', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'ui/main.js'));
 });
 
