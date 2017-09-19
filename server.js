ed5cfe291a3a9cf81dd7041433b578f00dcc09cf
makeanability <makeanability@gmail.com>
2017-09-19 23:49:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool=require('pg').pool;
 
-var config={
- user: 'makeanability',
- database: 'makeanability',
- host: 'db.imad.hasura-app.io',
- port: '5432',
- password: process.env.DB_PASSWORD
- 
-};
 
 
 
@@ -95,22 +86,7 @@ return html_template
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var pool=new pool(config);
-app.get('/test-db',function(req,res)
-{
-    pool.query('SELECT  * FROM user',function(err,rs)
-    {
-        if(err)
-        {
-            res.status(500).send(err.toString());
-        }
-        else
-        {
-            
-            res.send(JSON.stringify(rs));
-        }
-    });
-});
+
 
 
 
