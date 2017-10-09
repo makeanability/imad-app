343cddf4aae0c8c4b6e4525c33951e25081ac213
makeanability <makeanability@gmail.com>
2017-10-09 19:59:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ return html_template
 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'alias.html'));
 });
 
 var pool=new Pool(config);
